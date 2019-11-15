b466ecba1db8081c92e77f8d2984b29ac3d0fc8d
Flakes <ed.afram@gmail.com>
2019-11-15 19:43:15 -0300

    Inclusão de service para web aplicações.

---
+++ FluentAssociation/FluentAssociation.Library/Configuration/FluentAssociationBuilderExtension.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssociation.Library.Implementation;
+using FluentAssociation.Library.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FluentAssociation.Library.Configuration
+{
+    public static class FluentAssociationBuilderExtension
+    {
+        public static void AddFluentAssociation(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IFluentAssociation<>), typeof(FluentAssociation<>));
+        }
+    }
+}
