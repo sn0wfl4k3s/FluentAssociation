ee2a0f0b0129aff0f342dba157ca6438ef35494b
Flakes <ed.afram@gmail.com>
2020-11-16 22:16:38 -0300

    inclusão de documentação melhor no MinSuport.

--- FluentAssociation/FluentAssociation.Library/Interface/IFluentAssociation.cs
+++ FluentAssociation/FluentAssociation.Library/Interface/IFluentAssociation.cs
@@ -7,7 +7,9 @@ namespace FluentAssociation
     public interface IFluentAssociation<T>
     {
         /// <summary>
-        /// The minimum support for results in reports itemsets. The range should be from 0 to 1.
+        /// The minimum support for results in reports itemsets. 
+        /// The range should be from 0 to 1. 
+        /// The value is 0.2 by default. 
         /// </summary>
         float MinSuport { get; set; }
         /// <summary>
